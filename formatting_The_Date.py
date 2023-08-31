46c0c53df541d59fa40842f157ff62cb11a654c1
VijayaVijayan <141828367+VijayaVijayan@users.noreply.github.com>
2023-08-31 15:55:44 +0530

    Delete formatting_The_Date.py

--- formatting_The_Date.py
+++
@@ -1,8 +0,0 @@
-from dateutil.parser import parse
-def converting_Date():
-    date_string = input("Enter the date: ")
-    desired_format = input("Enter the desired format: ")
-    given_input = parse(date_string)
-    formatted_input = given_input.strftime(desired_format)
-    print(formatted_input)
-converting_Date()
